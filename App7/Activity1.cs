a91919596e9b285c22d5aac08e5d045695871b9e
Wiktor Jezioro <wikjezioro@op.pl>
2016-12-12 20:05:35 +0100

    Update Activity1.cs

--- App7/Activity1.cs
+++ App7/Activity1.cs
@@ -19,7 +19,7 @@ using System.Threading.Tasks;
 //using App7.Activity2;
 namespace App7
 {
-    [Activity(Label = "Lista zapowiedzianych testow wiadomoœci")]
+    [Activity(Label = "Lista zapowiedzianych testow wiadomoÅ“ci")]
     public class Activity1 : Activity
     {
         string user_name;
@@ -37,65 +37,27 @@ namespace App7
         {
             string CheckUrl = "http://google.com";
 
-         
-
             try
             {
-
-               
-
-
                 HttpWebRequest iNetRequest = (HttpWebRequest)WebRequest.Create(CheckUrl);
-
                 iNetRequest.Timeout = 3000;
-
                 WebResponse iNetResponse = iNetRequest.GetResponse();
-
-                // Console.WriteLine ("...connection established..." + iNetRequest.ToString ());
                 iNetResponse.Close();
-
                 return true;
-
             }
             catch (WebException ex)
             {
-
-                // Console.WriteLine (".....no connection..." + ex.ToString ());
-
                 return false;
             }
         }
-
-        ////////////////////////////////////////////
-
-      
-     
+        ///////////////////////////////////////////
         protected override void OnCreate(Bundle savedInstanceState)
         {
-
-
-          
-
-       
-
             base.OnCreate(savedInstanceState);
-
-
-
             SetContentView(Resource.Layout.layout1);
-
-          
-                var builder2 = new AlertDialog.Builder(this);
-            builder2.SetMessage("Trwa ³adowanie...");
-
-            //  dialog_loading = builder2.Create().Show();
-            //   builder2.Create();
+            var builder2 = new AlertDialog.Builder(this);
+            builder2.SetMessage("Trwa Â³adowanie...");
             dialog_loading = builder2.Show();
-        
-         //   RunOnUiThread(() => System.Threading.Thread.Sleep(2000));
-
-
-        
                 if (
                 CheckInternetConnection()
              )
@@ -104,30 +66,22 @@ namespace App7
                 dialog_loading.Dismiss();
                 dialog_loading.Cancel();
                 dialog_loading.Dispose();
-
-
             } else
             {
                 var builder = new AlertDialog.Builder(this);
-                builder.SetMessage("Nie masz po³aczenia z Internetem!");
+                builder.SetMessage("Nie masz poÂ³aczenia z Internetem!");
                 builder.SetPositiveButton("OK", (s, e) => { /* do something on OK click */ });
-
                 dialog_loading.Dismiss();
                 dialog_loading.Cancel();
                 dialog_loading.Dispose(); 
-
                 builder.Create().Show();
-
             }
 
             ///// GET DATA FROM NOTIFICATION CLICK /////
-
             // Get the message from the intent:
             string display_type = Intent.Extras.GetString("display_type", "");
             string test_id = Intent.Extras.GetString("test_id", "");
             //////////////////////////////////////////
-
-
             //retreive user name from preferences ////
             var prefs = Application.Context.GetSharedPreferences("KiedyTest", FileCreationMode.Private);
             var user_login = prefs.GetString("login", null);
@@ -135,50 +89,29 @@ namespace App7
             WebView localWebView = FindViewById<WebView>(Resource.Id.webView1);
             localWebView.Settings.JavaScriptEnabled = true;
             localWebView.SetWebViewClient(new WebViewClient()); // stops request going to Web Browser
-
             if (display_type == "all")
             {
                 localWebView.LoadUrl("http://dziennik.zs1debica.pl/kiedytest/android/show_planned_tests_for_user.php?display_type=all&ref_from=app_uczen&user_name=" + user_name);
             }
 
-
             if (display_type == "dzisiaj")
             {
                 localWebView.LoadUrl("http://dziennik.zs1debica.pl/kiedytest/android/show_planned_tests_for_user.php?display_type=dzisiaj&ref_from=app_uczen&user_name=" + user_name);
             }
-
-
             if (display_type == "jutro")
             {
                 localWebView.LoadUrl("http://dziennik.zs1debica.pl/kiedytest/android/show_planned_tests_for_user.php?display_type=jutro&ref_from=app_uczen&user_name=" + user_name);
             }
-
-
             if (display_type == "pojutrze")
             {
                 localWebView.LoadUrl("http://dziennik.zs1debica.pl/kiedytest/android/show_planned_tests_for_user.php?display_type=pojutrze&ref_from=app_uczen&user_name=" + user_name);
             }
-
-
             if (display_type == "onlyone")
             {
                 localWebView.LoadUrl("http://dziennik.zs1debica.pl/kiedytest/android/show_planned_test_details.php?apptype=student&user_name=" + user_name+"&test_id="+test_id);
             }
 
-
-
-
-
-
-
-
-
-
         }
-
-
-      
-
 
         public void onReceivedError(WebView view, int errorCod, Android.Resource.String description, Android.Resource.String failingUrl)
         {
@@ -191,13 +124,7 @@ namespace App7
                 RunOnUiThread(() => progressDialog.Hide());
             })).Start();
 
-            // Create your application here
         }
-
-
-   
-
-
     }
 }
 
